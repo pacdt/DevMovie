2ac4b872bcc0444b433b0d35af7e3a407cc0531e
Mateus Santos <mateus15do2@gmail.com>
2023-04-10 18:54:19 -0300

    Criação das pastas e do Home

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { Home } from './containers/Home/Index'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Home/>
   </React.StrictMode>,
 )
