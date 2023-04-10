3becec6e77412b38dfdf1204f47d92572a993e2d
Mateus Santos <mateus15do2@gmail.com>
2023-04-10 20:02:48 -0300

    Rotas Outlet & DefaultLayout

---
+++ src/components/Header/Index.jsx
@@ -0,0 +1,10 @@
+import Logo from '../../assets/logo.png'
+
+export default function Header(){
+    return(
+        <div>
+            <img src={Logo} alt='Logo DevMovies' style={{width: 500}}/>
+
+        </div>
+    )
+}
--- src/containers/Movies/Index.jsx
+++ src/containers/Movies/Index.jsx
@@ -1,3 +1,4 @@
+
 export default function Movies(){
     return(
         <div>
